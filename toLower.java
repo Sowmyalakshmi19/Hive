f30454f9c6ec40063808636a999de03c81b82070
Sowmyalakshmi19 <Sowmya1904@gmail.com>
2017-03-27 20:25:46 +0530

    Add files via upload

---
+++ toLower.java
@@ -0,0 +1,14 @@
+
+package udfhive;
+import org.apache.hadoop.hive.ql.exec.UDF;
+import org.apache.hadoop.io.Text;
+
+public class tolower extends UDF {
+	public Text evaluate(Text text) {
+		if (text==null) return null;
+		String str= text.toString().toLowerCase();
+		return new Text(str);
+
+}
+}
+
